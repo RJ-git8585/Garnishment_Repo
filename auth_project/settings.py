a0378785eeae185da91f312097e80fe695779c36
Ritik Jaiswal <ritikjaiswal8888@gmail.com>
2024-05-08 15:04:14 +0530

    2nd commit

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -85,6 +89,9 @@ REST_FRAMEWORK = {
 }
 
 
+
+
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
--- auth_project/urls.py
+++ auth_project/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('User/',include('User_app.urls')),
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-
+    path('User/',include('User_app.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
